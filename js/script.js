f3f910af50ae2acee746a512968150873c471cfc
Pratyush Ranjan <pratyushranjan368@gmail.com>
2021-02-08 22:43:58 +0530

    Modification-day-2: input & header done(styling)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var body = document.querySelector("body");
 var input1 = document.getElementById("input1");
 var input2 = document.getElementById("input2");
+var colorWrap = document.getElementsByClassName("input-color-wrap");
 var h3 = document.querySelector("h3");
 
-input1.addEventListener("input", function() {
-    body.style.background = "linear-gradient(to right, " + input1.value + ", " + input2.value + ")" ;
-})
+input1.addEventListener("input", function () {
+   body.style.background = "linear-gradient(to right, " + input1.value + ", " + input2.value + ")";
+});
 
-input2.addEventListener("input", function() {
-    body.style.background = "linear-gradient(to right, " + input1.value + ", " + input2.value + ")" ;
-    h3.appendChild(document.createTextNode("CSS Code is : background-image : linear-gradient(to right, " + input1.value + ", " + input2.value + ")"));
-})
-
+input2.addEventListener("input", function () {
+   body.style.background = "linear-gradient(to right, " + input1.value + ", " + input2.value + ")";
+   h3.appendChild(document.createTextNode("CSS Code is : background-image : linear-gradient(to right, " + input1.value + ", " + input2.value + ")"));
+});
