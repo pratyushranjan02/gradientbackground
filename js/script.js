ccc03c530ed7d2028d74a779ecae7e105bd53971
Pratyush Ranjan <pratyushranjan368@gmail.com>
2021-02-09 17:06:00 +0530

    functionality completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-var body = document.querySelector("body");
-var input1 = document.getElementById("input1");
-var input2 = document.getElementById("input2");
-var colorWrap = document.getElementsByClassName("input-color-wrap");
-var h3 = document.querySelector("h3");
+const body = document.querySelector("body");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const form = document.getElementById("input-form");
+const result = document.getElementById("result");
 
-input1.addEventListener("input", function () {
-   body.style.background = "linear-gradient(to right, " + input1.value + ", " + input2.value + ")";
+input1.addEventListener("input", (e) => {
+   e.target.parentElement.style.background = e.target.value;
 });
 
-input2.addEventListener("input", function () {
-   body.style.background = "linear-gradient(to right, " + input1.value + ", " + input2.value + ")";
-   h3.appendChild(document.createTextNode("CSS Code is : background-image : linear-gradient(to right, " + input1.value + ", " + input2.value + ")"));
+input2.addEventListener("input", (e) => {
+   e.target.parentElement.style.background = e.target.value;
+});
+
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+   result.textContent = `background-image : linear-gradient(to right, ${input1.value}, ${input2.value});`;
 });
