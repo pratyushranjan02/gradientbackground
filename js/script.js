a80acf4822168f0c90dbb789cb4b11df4e814083
Pratyush Ranjan <pratyushranjan368@gmail.com>
2021-02-10 22:51:43 +0530

    modification completed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-const body = document.querySelector("body");
+const mainSection = document.getElementById("main-section");
 const input1 = document.getElementById("input1");
 const input2 = document.getElementById("input2");
 const form = document.getElementById("input-form");
 const result = document.getElementById("result");
 
-input1.addEventListener("input", (e) => {
+const setGradient = (e) => {
    e.target.parentElement.style.background = e.target.value;
-});
+   mainSection.style.background = `linear-gradient(to right top, ${input1.value}, ${input2.value})`;
+};
 
-input2.addEventListener("input", (e) => {
-   e.target.parentElement.style.background = e.target.value;
-});
+input1.addEventListener("input", setGradient);
+
+input2.addEventListener("input", setGradient);
 
 form.addEventListener("submit", (e) => {
    e.preventDefault();
-   result.textContent = `background-image : linear-gradient(to right, ${input1.value}, ${input2.value});`;
+   result.textContent = `background-image : linear-gradient(to right top, ${input1.value}, ${input2.value});`;
+   mainSection.style.background = `linear-gradient(to right top, ${input1.value}, ${input2.value})`;
 });
